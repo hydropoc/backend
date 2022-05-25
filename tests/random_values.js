80e059c0c22950c1d1bd6daf7d223e0161cae973
Paul <pauul.win@gmail.com>
2022-05-25 12:14:42 +0200

    added test to fill database

---
+++ tests/random_values.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+// generate random number between two given values
+function randomNumber(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+console.log('Starting adding random data!');
+
+setInterval(() => {
+    axios
+        .post(
+            'http://localhost:3000/api/sensor/adddata',
+            JSON.stringify({
+                temperature_water: randomNumber(12, 14),
+                temperature_air: randomNumber(22, 25),
+                humidity: randomNumber(36, 40),
+                co2_level: Math.floor(randomNumber(500, 600)),
+                ph_value: Math.floor(randomNumber(5, 8)),
+                ec_value: Math.floor(randomNumber(2, 4)),
+                swimmer_1: 0,
+                swimmer_2: 0,
+                swimmer_3: Math.floor(randomNumber(900, 1000)),
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+        .then((res) => {
+            console.log('Added data!');
+        });
+}, 500);
