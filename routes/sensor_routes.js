c9b353bad42bd3fed21ad9b46170e01927648a20
Paul <pauul.win@gmail.com>
2022-05-25 12:15:12 +0200

    removed debug, edit get to post

--- routes/sensor_routes.js
+++ routes/sensor_routes.js
@@ -8,7 +8,7 @@ router.use((req, res, next) => {
     next();
 });
 
-router.get('/adddata', body('temperature_water').isNumeric(), body('temperature_air').isNumeric(), body('humidity').isNumeric(), body('co2_level').isNumeric(), body('ph_value').isNumeric(), body('ec_value').isNumeric(), body('swimmer_1').isNumeric(), body('swimmer_2').isNumeric(), body('swimmer_3').isNumeric(), (req, res) => {
+router.post('/adddata', body('temperature_water').isNumeric(), body('temperature_air').isNumeric(), body('humidity').isNumeric(), body('co2_level').isNumeric(), body('ph_value').isNumeric(), body('ec_value').isNumeric(), body('swimmer_1').isNumeric(), body('swimmer_2').isNumeric(), body('swimmer_3').isNumeric(), (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -20,8 +20,7 @@ router.get('/adddata', body('temperature_water').isNumeric(), body('temperature_
     database.sql.connect(database.sqlConfig).then((pool) => {
         pool.query('INSERT INTO [HydroPoc].[dbo].[sensordata] (temperature_water, temperature_air, humidity, co2_level, ph_value, ec_value, swimmer_1, swimmer_2, swimmer_3, timestamp) VALUES (' + req.body['temperature_water'] + ', ' + req.body['temperature_air'] + ', ' + req.body['humidity'] + ', ' + req.body['co2_level'] + ', ' + req.body['ph_value'] + ', ' + req.body['ec_value'] + ', ' + req.body['swimmer_1'] + ', ' + req.body['swimmer_2'] + ', ' + req.body['swimmer_3'] + ',' + timestamp + ')')
             .then((result) => {
-                console.log(result);
-                res.status(201).json({ success: true, timestamp });
+                return res.status(201).json({ success: true, timestamp });
             })
             .catch((insertError) => {
                 console.error(insertError);
