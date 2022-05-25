9d0f632b4b3db254a8dbc157bc77435f73a8346f
Paul Jongmans <pauul.win@gmail.com>
2022-05-26 01:49:59 +0200

    added two jobs

--- jobs/image_job.js
+++ jobs/image_job.js
@@ -1,14 +1,29 @@
-const database = require('./../database');
+const Raspistill = require('node-raspistill').Raspistill;
+const eventUtils = require('./../lib/eventUtils');
 
-const fileName = 'Pflanzenstatus Hauptkamera @ ' + new Date().getTime() + '.png';
+const camera = new Raspistill();
 
-database.sql.connect(database.sqlConfig).then((pool) => {
-    pool.query("INSERT INTO [HydroPoc].[dbo].[images] (data, timestamp) VALUES ('" + fileName + "', " + new Date().getTime() + ')')
-        .then((result) => {
-            console.log(result);
-            pool.close();
-        })
-        .catch((selectError) => {
-            console.log(selectError);
+camera
+    .takePhoto()
+    .then((photo) => {
+        console.log('photo taken, todo!');
+        console.log('add image to db');
+
+        eventUtils.addEvent('success', 'Picture taken');
+
+        /*
+        database.sql.connect(database.sqlConfig).then((pool) => {
+        pool.query("INSERT INTO [HydroPoc].[dbo].[images] (data, timestamp) VALUES ('" + fileName + "', " + new Date().getTime() + ')')
+            .then((result) => {
+                console.log(result);
+                pool.close();
+            })
+            .catch((selectError) => {
+                console.log(selectError);
+            });
         });
-});
+        */
+    })
+    .catch((error) => {
+        eventUtils.addEvent('error', 'Camera not found - check if camera is connected');
+    });
---
+++ jobs/plant_job.js
@@ -0,0 +1,16 @@
+const database = require('./../database');
+const gpioUtils = require('./../lib/gpioUtils');
+
+setTimeout(() => {
+    //check sensor values
+    //check if some shit is running
+    //check sensors
+    //toggle ports
+    //gpio utils can be used
+    //get current plant profile
+    //gpioUtils.togglePump(/* ID (1 - 5) and STATE (1 = ON, 0 = OFF) */);
+    //gpioUtils.toggleVentilation(/* STATE (1 = ON, 0 = OFF) */);
+    //gpioUtils.toggleLamp(/* STATE (1 = ON, 0 = OFF) */);
+    //gpioUtils.togglePumpFront(/* STATE (1 = ON, 0 = OFF) */);
+    //gpioUtils.togglePumpBack(/* STATE (1 = ON, 0 = OFF) */);
+}, 3000);
