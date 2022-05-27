841552f9ebcd6f4cd850800e21a7bb8ebb1224bc
Paul Jongmans <pauul.win@gmail.com>
2022-05-27 22:48:36 +0200

    added function to receive specific amount of logs

--- routes/device_routes.js
+++ routes/device_routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const os = require('os');
+const { body, validationResult } = require('express-validator');
+const database = require('./../database');
+const config = require('./../config');
 
 const router = express.Router();
 
@@ -8,7 +11,8 @@ router.use((req, res, next) => {
 });
 
 router.get('/informations', function (req, res) {
-    res.send({
+    res.status(200).send({
+        version: config.version,
         arch: os.arch(),
         cpus: os.cpus(),
         freememory: os.freemem(),
@@ -20,6 +24,25 @@ router.get('/informations', function (req, res) {
         totalmem: os.totalmem(),
         type: os.type(),
         uptime: os.uptime(),
+    });
+});
+
+router.get('/logs', body('amount').isInt({ min: 1, max: 100 }), (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    database.sql.connect(database.sqlConfig).then((pool) => {
+        pool.query('SELECT TOP ' + req.body.amount + ' * FROM [HydroPoc].[dbo].[events] ORDER BY id DESC')
+            .then((result) => {
+                return res.status(200).json(result.recordset);
+            })
+            .catch((selectError) => {
+                console.error(selectError);
+                return res.status(400).json({ error: 'data_error_reading_logs' });
+            });
     });
 });
 
