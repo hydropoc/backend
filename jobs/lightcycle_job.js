a55bf768d6c4d19b9e7ca2a1ca53e9746bf62e84
Paul Jongmans <pauul.win@gmail.com>
2022-06-03 00:00:42 +0200

    added check if time is between on program start

--- jobs/lightcycle_job.js
+++ jobs/lightcycle_job.js
@@ -5,6 +5,18 @@ const config = require('./../config');
 
 ora().succeed('[Jobs] Started Lightcycle job');
 
+const time = new Date();
+if (time.getHours() >= parseInt(config.lightcycle.startTime.split(':')[0]) && time.getHours() <= parseInt(config.lightcycle.endTime.split(':')[0])) {
+    if (time.getMinutes() >= parseInt(config.lightcycle.startTime.split(':')[1]) && time.getMinutes() <= parseInt(config.lightcycle.endTime.split(':')[1])) {
+        gpioUtils
+            .toggleLamp(true)
+            .then(() => {
+                ora().succeed("Set lamp to state 'on'");
+            })
+            .catch(() => undefined);
+    }
+}
+
 setInterval(() => {
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
