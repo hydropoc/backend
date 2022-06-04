775bb2230f71f1d83a1f665b0032a3f9e781f06f
Paul Jongmans <pauul.win@gmail.com>
2022-06-04 22:53:30 +0200

    added type

--- routes/device_routes.js
+++ routes/device_routes.js
@@ -27,7 +27,7 @@ router.get('/informations', function (req, res) {
     });
 });
 
-router.post('/logs', body('draw').optional().isNumeric(), body('search').optional().isObject(), body('length').optional().isNumeric(), body('start').optional().isNumeric(), (req, res) => {
+router.post('/logs', body('draw').optional().isNumeric(), body('type').optional().isString(), body('search').optional().isObject(), body('order').optional().isArray(), body('length').optional().isNumeric(), body('start').optional().isNumeric(), (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -37,7 +37,7 @@ router.post('/logs', body('draw').optional().isNumeric(), body('search').optiona
     database.sql.connect(database.sqlConfig).then((pool) => {
         pool.query('SELECT * FROM [HydroPoc].[dbo].[events] ORDER BY id DESC')
             .then((result) => {
-                const logs = { draw: req.body.draw != undefined ? req.body.draw : 1, recordsTotal: result.recordset.length, recordsFiltered: result.recordset.length, data: [] };
+                const logs = { draw: req.body['draw'] != undefined ? req.body['draw'] : 1, recordsTotal: result.recordset.length, recordsFiltered: 0, data: [] };
 
                 if (req.body['search'] == undefined) {
                     result.recordset.forEach((log) => {
@@ -59,12 +59,28 @@ router.post('/logs', body('draw').optional().isNumeric(), body('search').optiona
                             });
                         }
                     });
-
-                    logs.recordsFiltered = logs.data.length;
                 }
 
+                if (req.body['type'] != undefined) {
+                    switch (req.body['type']) {
+                        case 'success':
+                            logs.data = logs.data.filter((log) => log.type == 'success');
+                            break;
+                        case 'error':
+                            logs.data = logs.data.filter((log) => log.type == 'error');
+                            break;
+                        case 'info':
+                            logs.data = logs.data.filter((log) => log.type == 'info');
+                            break;
+                    }
+                }
+
+                console.log(req.body['order']);
+
+                logs.recordsFiltered = logs.data.length;
                 if (req.body['start'] != undefined) logs.data = logs.data.slice(req.body['start'], logs.data.length);
                 if (req.body['length'] != undefined) logs.data = logs.data.splice(0, req.body['length']);
+
                 return res.status(200).json(logs);
             })
             .catch((selectError) => {
