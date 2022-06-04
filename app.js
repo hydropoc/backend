d245870bcdfe2bd6f8ddc2976257ee2bf608f6d8
Paul Jongmans <pauul.win@gmail.com>
2022-06-04 15:43:24 +0200

    add bad json catch

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const deviceRoutes = require('./routes/device_routes');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({ origin: '*', allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'], allowHeaders: ['Content-Type', 'Accept'] }));
+app.use((error, req, res, next) => {
+    if (error instanceof SyntaxError) {
+        res.status(400).send({ error: 'Bad request' });
+    } else next();
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/sensor', sensorRoutes);
