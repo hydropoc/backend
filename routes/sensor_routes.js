30d7b2c0a5c37f0b0dec754bb24e4d1d240ee9c1
Paul Jongmans <pauul.win@gmail.com>
2022-06-11 02:10:39 +0200

    added route for sensor reading with time

--- routes/sensor_routes.js
+++ routes/sensor_routes.js
@@ -24,7 +24,6 @@ router.post('/adddata', body('temperature_water').isNumeric(), body('temperature
                 return res.status(201).json({ success: true, timestamp });
             })
             .catch((insertError) => {
-                console.error(insertError);
                 return res.status(400).json({ error: 'data_error_adding' });
             });
     });
@@ -94,13 +93,43 @@ router.post('/data', body('amount').optional().isInt({ min: 1, max: 1000 }), (re
                         return res.status(200).json(data);
                     })
                     .catch((selectError) => {
-                        console.error(selectError);
                         return res.status(400).json({ error: 'data_error_reading_activity' });
                     });
             })
             .catch((selectError) => {
-                console.error(selectError);
                 return res.status(400).json({ error: 'data_error_reading_sensor' });
+            });
+    });
+});
+
+router.post('/data_sensor', body('sensor').isString(), body('time').isInt({ min: 1, max: 86400 }), (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const data = {
+        average: 0,
+        sensors: [],
+        limits: config.limits[req.body['sensor']],
+    };
+
+    database.sql.connect(database.sqlConfig).then((pool) => {
+        pool.query('SELECT ' + req.body['sensor'] + ', timestamp FROM [HydroPoc].[dbo].[sensordata] WHERE timestamp > ' + (new Date().getTime() - req.body['time'] * 1000) + ' ORDER BY id DESC')
+            .then((result) => {
+                result.recordset.forEach((sensorData) => {
+                    sensorData['timestamp'] = parseInt(sensorData['timestamp']);
+                    data.average += sensorData[req.body['sensor']];
+                    data.sensors.push(sensorData);
+                });
+
+                data.average /= result.recordset.length;
+
+                return res.status(200).json(data);
+            })
+            .catch((selectError) => {
+                return res.status(400).json({ error: 'data_error_reading_sensor_timestamp' });
             });
     });
 });
