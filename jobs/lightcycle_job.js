453bf6897d4f22ead0afc4d00ba2705c0c231957
Paul Jongmans <pauul.win@gmail.com>
2022-06-12 03:50:36 +0200

    simplified and improved check

--- jobs/lightcycle_job.js
+++ jobs/lightcycle_job.js
@@ -1,43 +1,16 @@
 const ora = require('ora');
 const gpioUtils = require('./../lib/gpioUtils');
-const eventUtils = require('./../lib/eventUtils');
 const config = require('./../config');
 
 ora().succeed('[Jobs] Started Lightcycle job');
 
-checkIfTimeIsBetween();
 setInterval(() => {
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-    const currentMinute = currentTime.getMinutes();
-
-    const timeStartEquals = currentHour == parseInt(config.lightcycle.startTime.split(':')[0]) && currentMinute == parseInt(config.lightcycle.startTime.split(':')[1]);
-    const timeEndEquals = currentHour == parseInt(config.lightcycle.endTime.split(':')[0]) && currentMinute == parseInt(config.lightcycle.endTime.split(':')[1]);
-
-    if (timeStartEquals && timeEndEquals) {
-        ora().fail('Invalid time entered in lightcycle configuration');
-        eventUtils.addEvent('error', 'Invalid time entered in lightcycle configuration');
-        throw new Error('Invalid time entered in lightcycle configuration');
+    if (checkIfTimeIsBetween()) {
+        gpioUtils.setPinState(config.gpio['lamp_pin'], 1);
+    } else {
+        gpioUtils.setPinState(config.gpio['lamp_pin'], 0);
     }
-
-    if (timeStartEquals) {
-        gpioUtils
-            .toggleLamp(true)
-            .then(() => {
-                ora().succeed("Set lamp to state 'on'");
-            })
-            .catch(() => undefined);
-    }
-
-    if (timeEndEquals) {
-        gpioUtils
-            .toggleLamp(false)
-            .then(() => {
-                ora().succeed("Set lamp to state 'off'");
-            })
-            .catch(() => undefined);
-    }
-}, 60000); // Repeat every 60000 milliseconds (1 minute)
+}, 1000);
 
 function checkIfTimeIsBetween() {
     const time = new Date();
@@ -51,12 +24,5 @@ function checkIfTimeIsBetween() {
     timeEnd.setMinutes(parseInt(config.lightcycle.endTime.split(':')[1]));
     timeEnd.setSeconds(0);
 
-    if (time.getTime() >= timeStart.getTime() && time.getTime() <= timeEnd.getTime()) {
-        gpioUtils
-            .toggleLamp(true)
-            .then(() => {
-                ora().succeed("Set lamp to state 'on'");
-            })
-            .catch(() => undefined);
-    }
+    return time.getTime() >= timeStart.getTime() && time.getTime() <= timeEnd.getTime();
 }
