dd846d659f1dcc4734fd7527681c086dc976b774
Paul Jongmans <pauul.win@gmail.com>
2022-06-12 03:57:33 +0200

    added notes 2

--- jobs/plant_job.js
+++ jobs/plant_job.js
@@ -11,7 +11,13 @@ setInterval(() => {
     //toggle ports
     //gpio utils can be used
     //get current plant profile
-    //gpioUtils.togglePump(/* ID (1 - 5) and STATE (1 = ON, 0 = OFF) */);
+    //
+    //gpioUtils.setPinState(config.gpio['pump_small_1'], 0 or 1);
+    //gpioUtils.setPinState(config.gpio['pump_small_2'], 0 or 1);
+    //gpioUtils.setPinState(config.gpio['pump_small_3'], 0 or 1);
+    //gpioUtils.setPinState(config.gpio['pump_small_4'], 0 or 1);
+    //gpioUtils.setPinState(config.gpio['pump_small_5'], 0 or 1);
+    //
     //gpioUtils.setPinState(config.gpio['fan_pin'], 0 or 1);
     //gpioUtils.setPinState(config.gpio['lamp_pin'], 0 or 1);
     //gpioUtils.setPinState(config.gpio['pump_front'], 0 or 1);
