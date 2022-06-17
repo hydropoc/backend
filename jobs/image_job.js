882a997722317c3744c75d12a2fc4921e5638b8c
Paul J <56239171+paul-jongmans@users.noreply.github.com>
2022-06-17 11:08:18 +0200

    created const for camera object

--- jobs/image_job.js
+++ jobs/image_job.js
@@ -5,25 +5,28 @@ const config = require('./../config.json');
 
 ora().succeed('[Jobs] Started Image job');
 
+const camera = new Raspistill({
+    encoding: 'png',
+    fileName: '',
+});
+
 takePhoto();
 setInterval(() => {
     takePhoto();
 }, config.pictureDelay * 1000);
 
 function takePhoto() {
-    const fileName = ('Wachstum ' + new Date().toLocaleString('de-DE').toString()).replaceAll(':', '-');
+    camera.setOptions({
+        fileName: ('Wachstum ' + new Date().toLocaleString('de-DE').toString()).replaceAll(':', '-'),
+    });
 
-    new Raspistill({
-        encoding: 'png',
-        fileName,
-    })
+    camera
         .takePhoto()
         .then((photo) => {
             const nextPicture = new Date();
             nextPicture.setSeconds(nextPicture.getSeconds() + config.pictureDelay);
 
-            ora().succeed('[Jobs] Created photo! New photo will be created at ' + nextPicture.toLocaleString('de-DE').toString());
-            eventUtils.addEvent('success', 'Picture taken');
+            eventUtils.addEvent('success', 'Created photo! New photo will be created at ' + nextPicture.toLocaleString('de-DE').toString());
         })
-        .catch((error) => eventUtils.addEvent('error', 'Camera not found - check if camera is connected'));
+        .catch((error) => undefined);
 }
